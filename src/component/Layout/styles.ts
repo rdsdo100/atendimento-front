66564e0a288ddb6182d692c19d61d126ea16217c
Rubens Diego <rdsdo2011@gmail.com>
2020-10-08 08:47:49 -0300

    add

--- src/component/Layout/styles.ts
+++ src/component/Layout/styles.ts
@@ -7,4 +7,18 @@ import styled from 'styled-components';
  * CT = Content
  */
 
-export const Grid = styled.div``;
+export const Grid = styled.div`
+
+
+ display: grid;
+    grid-template-columns: 40px auto;
+    grid-template-rows: 40px auto ;
+
+    grid-template-areas:
+    'MH MH'
+    'MH CT';
+
+    height: 100vh;
+   
+
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
-
+import Layout from '../component/Layout'
 import Input from '../component/Input'
 
  const Home: React.FC = () => {
 
 return(
      <div>
-      
+      <Layout>
 
-         <Input></Input>
-
-
+      </Layout>  
      </div>
 )
 }
