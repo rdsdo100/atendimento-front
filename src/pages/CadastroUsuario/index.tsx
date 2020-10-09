dce4c625093e3c0ba8bb73bd36ac95b020825236
Rubens Diego <rdsdo2011@gmail.com>
2020-10-08 22:39:08 -0300

    craindo cadastro de usuarios - formularios

--- src/pages/CadastroUsuario/index.tsx
+++ src/pages/CadastroUsuario/index.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import CardForms from "../../component/CardForms";
 import Input from '../../component/Input'
-
+import {Container , ContainerInpus} from './styles'
 
 
 const CadastroUsuario: React.FC = () => {
     return(
+<Container>
+        <CardForms>
+            <Input placeholder= "Nome"></Input>
+            <Input placeholder= "E-mail"></Input>
+            <Input placeholder= "Matricula"></Input>
+            <Input placeholder= "Grupo usuário"></Input>
+            <ContainerInpus>
+            <Input placeholder= "Senha" type='password'></Input>
+            <Input placeholder= "Nova senha" type='password'></Input>
+            </ContainerInpus>
 
-            <CardForms>
-<Input placeholder= "Nome"></Input>
-<Input placeholder= "E-mail"></Input>
-<Input placeholder= "Senha"></Input>
-<Input placeholder= "Nova senha"></Input>
-<Input placeholder= "Matricula"></Input>
-<Input placeholder= "Grupo usuário"></Input>
-</CardForms>
 
+        </CardForms>
+</Container>
     )
 }
 export default CadastroUsuario
--- src/pages/CadastroUsuario/styles.ts
+++ src/pages/CadastroUsuario/styles.ts
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+ position: relative;
+  margin: 0 auto;
 
    
+`;
+export const ContainerInpus = styled.div`
+  
+display: flex;
+   flex-direction: row;
+ 
+ 
 `;
