c5c9d1c23d79a8609cc5440e881d2d2b83cc30a2
Rubens Diego <rdsdo2011@gmail.com>
2021-03-16 20:50:28 -0300

    Refatorendo

--- src/component/graficos/CardPieChart/index.tsx
+++ src/component/graficos/CardPieChart/index.tsx
@@ -39,20 +39,10 @@ const CardPieChart: React.FC = () => {
           name: '40-49',
           uv: 8.63,
           pv: 3908,
-          fill: '#a4de6c',
+          fill: '#31224b',
         },
-        {
-          name: '50+',
-          uv: 2.63,
-          pv: 4800,
-          fill: '#d0ed57',
-        },
-        {
-          name: 'unknow',
-          uv: 6.67,
-          pv: 4800,
-          fill: '#f80058',
-        },
+        
+       
       ];
       
     return (
