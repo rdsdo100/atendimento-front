33bf8b6476db906afde3e65f34dd4e6552d95065
Rubens Diego <rdsdo2011@gmail.com>
2021-03-17 16:14:54 -0300

    Refatorendo

--- src/pages/Empresas/index.tsx
+++ src/pages/Empresas/index.tsx
@@ -1,5 +1,6 @@
 import React, { FormEvent } from 'react';
 import InputCadastro from '../../component/inputs/InputCadastro';
+import InputId from '../../component/inputs/InputId';
 import LayoutPrincipal from '../../component/LayoutPrincipal';
 import { CardListTab, Tabs } from '../../component/TabsComponents';
 import Tab from '../../component/TabsComponents/Tab';
@@ -26,7 +27,39 @@ const Empresas: React.FC = () => {
         event.preventDefault()
 
         alert("Ta indo!!!!")
+        limparCampos()
 
+    }
+
+    function limparCampos() {
+
+        if ((document.getElementById("idEmpresa")) &&
+            (document.getElementById("codigoEmpresa")) &&
+            (document.getElementById("nomeEmpresa"))) {
+
+            (document.getElementById("codigoEmpresa") as HTMLInputElement).value = "";
+            (document.getElementById("nomeEmpresa") as HTMLInputElement).value = "";
+            (document.getElementById("idEmpresa") as HTMLInputElement).value = "";
+        }
+    }
+
+    function carregarCampos(idEmpresa: string, codigoEmpresa: string, nomeEmpresa: string) {
+
+        if ((document.getElementById("idEmpresa")) &&
+            (document.getElementById("codigoEmpresa")) &&
+            (document.getElementById("nomeEmpresa"))) {
+
+            (document.getElementById("codigoEmpresa") as HTMLInputElement).value = codigoEmpresa;
+            (document.getElementById("nomeEmpresa") as HTMLInputElement).value = nomeEmpresa;
+            (document.getElementById("idEmpresa") as HTMLInputElement).value = idEmpresa;
+        }
+
+        if ((document.getElementById("tab1Empresas")) &&
+            (document.getElementById("tab2Empresas"))) {
+            (document.getElementById("tab1Empresas") as HTMLInputElement).checked = true;
+            (document.getElementById("tab2Empresas") as HTMLInputElement).checked = false;
+
+        }
     }
 
     return (
@@ -41,9 +74,9 @@ const Empresas: React.FC = () => {
 
                         <Form onSubmit={handleSubmit} >
 
-
-                            <InputCadastro>Codigo da Empresa</InputCadastro>
-                            <InputCadastro>Nome da Empresa</InputCadastro>
+                            <InputId id='idEmpresa' >Id</InputId>
+                            <InputCadastro id='codigoEmpresa'>Codigo da Empresa</InputCadastro>
+                            <InputCadastro id='nomeEmpresa' >Nome da Empresa</InputCadastro>
 
                             <button type="submit" >Enviar</button>
 
