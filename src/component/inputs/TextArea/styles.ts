fe738ebb45620024a6c88d5e8f1c11a0a5324358
Rubens Diego <rdsdo2011@gmail.com>
2021-03-20 01:52:49 -0300

    Refatorando

--- src/component/inputs/TextArea/styles.ts
+++ src/component/inputs/TextArea/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.textarea`
     width: 100%;
-    height: 350px;
+    height: 250px;
     border: #333333 1px solid;
     margin: 7px 0;
     padding: 10px;
