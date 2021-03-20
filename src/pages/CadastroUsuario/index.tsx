15c8c8fc34a87bfee0e8339657f49ebace597391
Rubens Diego <rdsdo2011@gmail.com>
2021-03-20 02:18:15 -0300

    Refatorando

--- src/pages/CadastroUsuario/index.tsx
+++ src/pages/CadastroUsuario/index.tsx
@@ -1,13 +1,18 @@
 
 import React, { FormEvent, ChangeEvent, useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import CardUsuario from '../../component/CardUsuario';
+import { Button } from '../../component/CardUsuario/styles';
 import InputCadastro from '../../component/inputs/InputCadastro';
 import Select from '../../component/inputs/Select';
 import LayoutPrincipal from '../../component/LayoutPrincipal';
 import { CardListTab, Tab, Tabs } from '../../component/TabsComponents';
 import { api } from '../../services/api';
-import { Container } from './styles'
+import { Container, DivuttonEnviar, DivButtonEditar } from './styles'
+
+
+interface IButton {
+    display?: string
+}
 
 interface IGrupoUsuario {
     id: number
@@ -15,6 +20,7 @@ interface IGrupoUsuario {
 }
 
 interface IUsuario {
+    id?: number
     nome: string
     email: string
     senha: string
@@ -23,7 +29,7 @@ interface IUsuario {
 
 }
 
-interface IGetUsuario{
+interface IGetUsuario {
     id: number
     nomeUsuario?: string,
     email?: string,
@@ -36,16 +42,21 @@ interface IGetUsuario{
 
 const CadastroUsuario: React.FC = () => {
 
-    const history = useHistory()
-    const auth = localStorage.getItem("Authorization")
-    const [gupoUsuarios, setGrupoUsuarios] = useState<IGrupoUsuario[]>([])
-    const [usuarios, setUsuarios] = useState<IGetUsuario[]>([])
+    const [listUsuarios, setListUsuarios] = useState<IGetUsuario[]>([])
+    const [message, setMessage] = useState<string>('')
+    const [idDeleteUsuario, setIdDeleteUsuario] = useState<number>()
+    const [idEditUsuario, setIdEditUsuario] = useState<number>()
+    const [idCadastroUsuario, setIdCadastroUsuario] = useState<number>()
+    const [butonEnviar, setButtonEnviar] = useState<IButton>({ display: 'flex' })
+    const [butonEdit, setButtonEdit] = useState<IButton>({ display: 'none' })
     const [nome, setNome] = useState<string>('')
     const [email, setEmail] = useState<string>('')
     const [matricula, setMatricula] = useState<string>('')
     const [grupoUsuario, setGrupoUsuario] = useState<number>(0)
     const [password, setPassword] = useState<string>('')
-
+    const [gupoUsuarios, setGrupoUsuarios] = useState<IGrupoUsuario[]>([])
+    const auth = localStorage.getItem("Authorization")
+ 
     useEffect(() => {
 
         api.get<IGrupoUsuario[]>('gupo-usuario',
@@ -55,28 +66,28 @@ const CadastroUsuario: React.FC = () => {
                 setGrupoUsuarios(resposta)
             }).catch(erro => {
                 alert('Erro ao acessar servidor!')
-                history.push('/')
+               
             })
 
     }, [])
 
-useEffect(()=>{
+    useEffect(() => {
 
-    api.get<IGetUsuario>('user', 
-    { headers: { authorization: auth } })
-    .then(response => {
-        const resposta: any = response.data
+        api.get<IGetUsuario>('user',
+            { headers: { authorization: auth } })
+            .then(response => {
+                const resposta: any = response.data
 
-       setUsuarios(resposta)
-       
+                setListUsuarios(resposta)
 
-    })
-    .catch(erro => {
-        alert('Erro ao acessar servidor!')
-        //  history.push('/')
-    })
 
-},[])
+            })
+            .catch(erro => {
+                alert('Erro ao acessar servidor!')
+                
+            })
+
+    }, [])
 
 
     async function handleSubmit(event: FormEvent) {
@@ -110,19 +121,19 @@ useEffect(()=>{
     }
 
     function handleImputNome(event: ChangeEvent<HTMLInputElement>) {
-        const nome  = event.target.value
+        const nome = event.target.value
 
         setNome(String(nome))
 
 
     }
     function handleImputEmail(event: ChangeEvent<HTMLInputElement>) {
-        const emailUsuario  = event.target.value
+        const emailUsuario = event.target.value
         setEmail(String(emailUsuario))
 
     }
     function handleImputMatricula(event: ChangeEvent<HTMLInputElement>) {
-        const matricula  = event.target.value
+        const matricula = event.target.value
         setMatricula(String(matricula))
 
     }
@@ -136,6 +147,140 @@ useEffect(()=>{
         setPassword(String(value))
 
     }
+
+  /*  function limparCampos() {
+
+        if ((document.getElementById("id_empresa")) &&
+            (document.getElementById("codigo_empresas")) &&
+            (document.getElementById("nome_empresas"))) {
+
+            (document.getElementById("id_empresa") as HTMLInputElement).value = "";
+            (document.getElementById("codigo_empresas") as HTMLInputElement).value = "";
+            (document.getElementById("nome_empresas") as HTMLInputElement).value = "";
+        }
+    }
+*/
+/*
+    function carregarCampos(idEmpresa: string, codigoEmpresa: string, nomeEmpresa: string) {
+
+        if ((document.getElementById("id_empresa")) &&
+            (document.getElementById("codigo_empresas")) &&
+            (document.getElementById("nome_empresas"))) {
+
+            (document.getElementById("id_empresa") as HTMLInputElement).value = idEmpresa;
+            (document.getElementById("codigo_empresas") as HTMLInputElement).value = codigoEmpresa;
+            (document.getElementById("nome_empresas") as HTMLInputElement).value = nomeEmpresa;
+
+        }
+
+        if ((document.getElementById("tab1Usuario")) &&
+            (document.getElementById("tab2Usuario"))) {
+            (document.getElementById("tab1Usuario") as HTMLInputElement).checked = true;
+            (document.getElementById("tab2Usuario") as HTMLInputElement).checked = false;
+
+        }
+    }
+    */
+
+        /*
+        const handleDelete = (idDelete: number) => {
+    
+            api.delete<string>(`empresa/${idDelete}`,
+                { headers: { authorization: auth } })
+                .then(response => {
+                    const resposta: any = response.data
+    
+                    setMessage(String(resposta))
+                    setIdDeleteEmpresa(idDelete)
+    
+                }).catch(erro => {
+    
+                    alert('Não enviado!')
+    
+                })
+        }
+    
+       */
+
+
+    /*
+        function updateEmpresa(idEdit: number) {
+    
+            const ativarButton: IButton = { display: 'flex' }
+            const desativarButton: IButton = { display: 'none' }
+            setButtonEdit(ativarButton)
+            setButtonEnviar(desativarButton)
+    
+    
+    
+            const empresaEdit = listEmpresas.find(item => item.id === idEdit)
+    
+    
+    
+    
+            carregarCampos(
+                String(empresaEdit?.id),
+                String(empresaEdit?.codigoEmpresa),
+                String(empresaEdit?.nomeEmpresa)
+                )
+    
+        }*/
+
+
+    /*   const handlePut = () => {
+   
+           let putEmpresa: IEmpresas
+   
+           if ((document.getElementById("id_empresa"))
+               && (document.getElementById("codigo_empresas"))
+               && (document.getElementById("nome_empresas"))) {
+               putEmpresa = {
+                   nomeEmpresa: String((document.getElementById("nome_empresas") as HTMLInputElement).value),
+                   codigoEmpresa: String((document.getElementById("codigo_empresas") as HTMLInputElement).value),
+                   id: Number((document.getElementById("id_empresa") as HTMLInputElement).value),
+               }
+   
+           
+   
+   
+               api.put(`empresa`, putEmpresa,
+                   { headers: { authorization: auth } })
+                   .then(response => {
+                       const resposta: any = response.data
+   
+                       const ativarButton: IButton = { display: 'flex' }
+                       const desativarButton: IButton = { display: 'none' }
+                       setButtonEdit(desativarButton)
+                       setButtonEnviar(ativarButton)
+   
+                       limparCampos()
+   
+                       setMessage(String(resposta))
+                      
+                       setIdEditEmpresa(putEmpresa.id)
+   
+                   }).catch(erro => {
+   
+                       alert('Não enviado!')
+   
+                   })
+           }
+       }
+   
+   */
+  /*
+
+    function handleCancelar() {
+
+        const ativarButton: IButton = { display: 'flex' }
+        const desativarButton: IButton = { display: 'none' }
+        setButtonEdit(desativarButton)
+        setButtonEnviar(ativarButton)
+
+        limparCampos()
+
+    }
+*/
 
     return (
         <Container>
@@ -187,22 +332,31 @@ useEffect(()=>{
                                 Cadastrar
                          </button>
 
+                            <DivuttonEnviar style={butonEnviar} >
+                                <Button style={{ background: "green" }} type="submit" >Enviar</Button>
+                            </DivuttonEnviar>
+                            <DivButtonEditar style={butonEdit}>
+                                <Button style={{ background: "yellow", color: '#000' }} type="button" onClick={() => { }} >Editar</Button>
+                                <Button type="button" style={{ background: "#e44c4e", color: '#bfbfbf' }} onClick={() => { }}  >Cancelar</Button>
+                            </DivButtonEditar>
+
+
                         </form>
                     </Tabs>
                     <Tabs IdNameTab="tab2Usuario" text='Lista de Usuarios'>
                         <CardListTab>
-{
-    usuarios.map((user:any)=>{
+                            {
+                                listUsuarios.map((user: any) => {
 
-        return <CardUsuario
-        key={user.id}
-        id={user.id}
-        nomeUsuario={user.nomeUsuario} 
-        matricula = {user.matricula}
-        />
-    })
+                                    return <CardUsuario
+                                        key={user.id}
+                                        id={user.id}
+                                        nomeUsuario={user.nomeUsuario}
+                                        matricula={user.matricula}
+                                    />
+                                })
 
-}
+                            }
                         </CardListTab>
                     </Tabs>
 
--- src/pages/CadastroUsuario/styles.ts
+++ src/pages/CadastroUsuario/styles.ts
@@ -7,3 +7,27 @@ height: 100vh;
   background-color:  ${props => props.theme.colors.primary};
   
 `;
+
+
+export const DivuttonEnviar = styled.div`
+ 
+ position: relative;
+
+ button{
+  padding: 0px 20px;
+  width: 100%;
+ 
+ }
+`;
+export const DivButtonEditar = styled.div`
+ 
+ position: relative;
+ display: flex;
+ margin: 10px;
+ 
+ button{
+  margin: 0px 10px;
+  width: 100%;
+ }
+ 
+`;
