01349e5ed554ab3262a3557cebfbd43531d861a9
Rubens Diego <rdsdo2011@gmail.com>
2021-03-24 13:09:16 -0300

    Refatorando usuarios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,6 @@
 import axios from "axios"
 
-const local =  "http://localhost:3333"
-const heroku= 'https://atendimento-back.herokuapp.com'
-
-const url = heroku
+const url = process.env.REACT_APP_SERVER_ACESS
 
 const api = axios.create({
     baseURL: url,
